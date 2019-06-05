1474c72851fe5fdc5b77572f382bb59bfdd51c8e
Brad <bjkapteyn@gmail.com>
2019-06-05 14:34:32 -0400

    First Commit

---
+++ RecipePuppy/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RecipePuppy.Models;
+
+namespace RecipePuppy.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RecipeList(string ing1, string ing2, int num)
+        {
+            List<Recipe> Recipes = RecipeAPIDAL.GetNewRecipe(ing1, ing2, num);
+
+            return View(Recipes);
+        }
+    }
+}
---
+++ RecipePuppy/Models/RecipeAPIDAL.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace RecipePuppy.Models
+{
+    public class RecipeAPIDAL
+    {
+        public static string APICall(string URL)
+        {
+            HttpWebRequest request = WebRequest.CreateHttp(URL);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            string APIText = rd.ReadToEnd();
+
+            return APIText;
+        }
+
+        public static List<Recipe> GetNewRecipe(string ing1, string ing2, int num)
+        {
+            string URL = $"http://www.recipepuppy.com/api/?i={ing1},{ing2}&p={num}";
+
+            string APIText = APICall(URL);
+
+            JToken json = JToken.Parse(APIText);
+
+            List<JToken> jsonRecipes = json["results"].ToList();
+
+            List<Recipe> Recipes = new List<Recipe>();
+
+            foreach(JToken j in jsonRecipes)
+            {
+                Recipe r = new Recipe(j);
+                Recipes.Add(r);
+            }
+
+            return Recipes;
+        }
+    }
+}
