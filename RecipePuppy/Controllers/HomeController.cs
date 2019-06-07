ea5c44a6801dd062477528b66653a187f814f8bc
Brad <bjkapteyn@gmail.com>
2019-06-07 11:38:36 -0400

    Fixed bug in add to favorite list, added favorite list view

--- RecipePuppy/Controllers/HomeController.cs
+++ RecipePuppy/Controllers/HomeController.cs
@@ -15,9 +15,9 @@ namespace RecipePuppy.Controllers
             return View();
         }
 
-        public ActionResult RecipeList(string ing1, string ing2, int num)
+        public ActionResult RecipeList(string ing1, string ing2/*, int num*/)
         {
-            List<Recipe> Recipes = RecipeAPIDAL.GetNewRecipe(ing1, ing2, num);
+            List<Recipe> Recipes = RecipeAPIDAL.GetNewRecipe(ing1, ing2/*, num*/);
 
             return View(Recipes);
         }
@@ -29,16 +29,33 @@ namespace RecipePuppy.Controllers
         public ActionResult RecipeFavorite(string title, string image, string ingredients, string thumbNail)
         {
             User u = (User)Session["User"];
+            User favU = db.Users.Where(login => login.UserID == u.UserID).ToList().First();
             Favorite fav = new Favorite();
+            //favU.Email = u.Email;
+            //favU.Favorites = u.Favorites;
+            //favU.Password = u.Password;
+            //favU.UserName = u.UserName;
+            //favU.UserID = u.UserID;
+            //favU.Name = u.Name;
             fav.Title = title;
             fav.Image = image;
             fav.Ingredients = ingredients;
             fav.Thumbnail = thumbNail;
             fav.FavUserID = u.UserID;
-            fav.User = u;
+            fav.User = favU;
+            
             db.Favorites.Add(fav);
             db.SaveChanges();
-            return RedirectToAction("RecipeList");
+            return RedirectToAction("FavoriteList");
+        }
+
+        public ActionResult FavoriteList()
+        {
+            User u = (User)Session["User"];
+            List<Favorite> favs = db.Favorites.Where(fav => fav.FavUserID == u.UserID).ToList();
+
+
+            return View(favs);
         }
 
         public ActionResult Register()
@@ -62,12 +79,34 @@ namespace RecipePuppy.Controllers
         [HttpPost]
         public ActionResult Login(User u)
         {
-           
-            User user = db.Users.Where(login => login.UserName == u.UserName && login.Password == u.Password).ToList().First();
+            List < User > users = db.Users.ToList();
+            User user = new User();
+            for (int i = 0; i < users.Count; i++)
+            {
+                if (u.UserName == users[i].UserName)
+                {
+                    if (u.Password == users[i].Password)
+                    {
+                        Session["UserError"] = "";
+                        user = users[i];
+                        Session["User"] = user;
+                    }
+                    else
+                    {
+                        Session["UserError"] = "Incorrect username or password";
+                        return RedirectToAction("~/Home/Login");
+                    }
+                }
+                else
+                {
+                    Session["UserError"] = "Incorrect username or password";
+                    return RedirectToAction("Login");
+                }
+            }
+            //User user = db.Users.Where(login => login.UserName == u.UserName && login.Password == u.Password).ToList().First();
+            //Session["User"] = (User)user;
 
-            Session["User"] = (User)user;
-
-            return View("RecipeList");
+            return View("Ingredients");
         }
 
 
